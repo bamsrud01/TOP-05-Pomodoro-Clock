7f1cdcd72447f245e48f6a80a8e3c89947d32b4e
Barrett Amsrud <barrettamsrud@gmail.com>
2017-05-23 06:27:18 -0500

    Buttons to add and subtract time

--- app.js
+++ app.js
@@ -1,5 +1,25 @@
 $(document).ready(() => {
 
-  console.log('jQuery loaded');
-  
+  //  Default times in seconds
+  let workTime = 1500;
+  let breakTime = 300;
+  let isRunning = false;
+
+  $('.time').on('click', '.subtract', function() {
+    let $time = $(this).parent().find('h3').children();
+    let newTime = $time.text() - 1;
+    $time.text(newTime);
+  });
+
+  $('.time').on('click', '.add', function() {
+    let $time = $(this).parent().find('h3').children();
+    let newTime = parseInt($time.text()) + 1;
+    $time.text(newTime);
+  });
+
 });
+
+//  Function to add time to clock
+function addTime(clock, time) {
+
+}
