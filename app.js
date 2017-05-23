c3b5b935ad7d978690d2e7b1fc84bc6bbe1d2913
Barrett Amsrud <barrettamsrud@gmail.com>
2017-05-23 06:59:11 -0500

    Correctly manipulated times

--- app.js
+++ app.js
@@ -1,25 +1,54 @@
+//  Default times in seconds
+let workTime = 1500;
+let breakTime = 300;
+let isRunning = false;
+
+//  jQuery event handlers
 $(document).ready(() => {
 
-  //  Default times in seconds
-  let workTime = 1500;
-  let breakTime = 300;
-  let isRunning = false;
+  //  Set up initial times
+  function setUpClock() {
+    $('#work-time').text(workTime / 60);
+    $('#break-time').text(breakTime / 60);
+  }
 
+  //  Handle subtract time button
   $('.time').on('click', '.subtract', function() {
     let $time = $(this).parent().find('h3').children();
-    let newTime = $time.text() - 1;
-    $time.text(newTime);
+    changeTime($time, -1);
   });
 
+  //  Handle add time button
   $('.time').on('click', '.add', function() {
     let $time = $(this).parent().find('h3').children();
-    let newTime = parseInt($time.text()) + 1;
-    $time.text(newTime);
+    changeTime($time, 1);
   });
+
+  //  Start and stop clock
+  $('#buttons').on('click', '#play-pause', function() {
+    console.log('clicked');
+    if (!isRunning) {
+      console.log(workTime);
+      console.log(breakTime);
+      // isRunning = true;
+      //  Start clock
+    } else {
+
+    }
+  });
+
+  setUpClock();
 
 });
 
-//  Function to add time to clock
-function addTime(clock, time) {
-
+//  Function to change work or rest time
+function changeTime($time, timeChange) {
+  if (!isRunning && ($time.text() >= 1 || timeChange >= 1)) {
+    let newTime = parseInt($time.text()) + timeChange;
+    $time.text(newTime);
+    workTime = $('#work-time').text() * 60;
+    breakTime = $('#break-time').text() * 60;
+    console.log('Inside', workTime);
+    console.log('Inside', breakTime);
+  }
 }
